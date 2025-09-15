c0fa3a4c272c37f9a6e0515e32d30f9629108d6c
zhushiquan <zhushiquan@seevision.cn>
2025-09-15 11:06:24 +0800

    upadate Sep 15

--- kenbuild.sh
+++ kenbuild.sh
@@ -2,9 +2,9 @@
 #  基础配置：默认架构（可手动指定 ARCH=arm64 切换）
 #    使用方法：make ARCH=arm32  # 编译arm32
 #             make ARCH=arm64  # 编译arm64
-
+make ARCH=arm64 clean
 make ARCH=arm64
 make ARCH=arm64 install
 
-make ARCH=arm32
-make ARCH=arm32 install
+# make ARCH=arm32
+# make ARCH=arm32 install
